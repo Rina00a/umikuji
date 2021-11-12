be44d78109e7c168edfcf297943dcf8a4de49e09
Arakaki Rina <hare00aaa@gmail.com>
2021-11-12 14:35:48 +0900

    HTMLでボタンと結果の箱作成、JSで表示、非表示を書き込み

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$("#box-btn").on("click",function(){
+    $(".result").animate({opacity:1},500);
+})
+$("#box-btn").on("click",function(){
+    $(".result").animate({opacity:0},500);
+})
+
+// 右クリック→競合を解決→アド・コミット
