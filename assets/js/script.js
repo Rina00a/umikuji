44f76d0deca27a553581dc923df24fdbe553af39
Arakaki Rina <hare00aaa@gmail.com>
2021-11-12 15:41:58 +0900

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-$("#box-btn").on("click",function(){
-    $(".result").animate({opacity:1},500);
+$(function(){
+    $("#box-btn").on("click",function(){
+        $(".result").animate({opacity:1},500);
+    })
+    $("#box-btn").on("click",function(){
+        $(".result").animate({opacity:0},500);
+    })
 })
-$("#box-btn").on("click",function(){
-    $(".result").animate({opacity:0},500);
-})
-
 // 右クリック→競合を解決→アド・コミット
+// JQuery　CDN 
